7dac83005d5b588e4265dcc68fa55601e301f372
alaminyusuf <alaminyusuf131@gmail.com>
2026-05-05 12:47:13 +0100

    feat: Add rate limiting to API Gateway

--- api-gateway/src/server/startServer.js
+++ api-gateway/src/server/startServer.js
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 
 import resolvers from '#root/graphql/resolvers';
 import typeDefs from '#root/graphql/typeDefs';
@@ -20,6 +21,15 @@ const apolloServer = new ApolloServer({
 });
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 
 app.use(cookieParser());
 app.use(
